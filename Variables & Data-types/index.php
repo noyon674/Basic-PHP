eeafd08841f70d0f8d2541d0cacd940749a3b4df
Noyon sarker <124178299+noyon674@users.noreply.github.com>
2023-05-13 11:07:12 +0600

    Create index.php

---
+++ Variables & Data-types/index.php
@@ -0,0 +1,24 @@
+<?php
+// Variable Declearation
+$name = 'Noyon';
+$age = 23;
+
+echo "Name: {$name} <br> Age: {$age} <br>";
+
+////Data Type
+$string = "Hello World";
+echo var_dump($string);
+echo "<br>";
+
+$int = 20;
+echo var_dump($int);
+echo "<br>";
+
+$float = 20.40;
+echo var_dump($float);
+echo "<br>";
+
+$persons = array("Noyon", "Mithun", "Purna", "Ope");
+echo var_dump($persons);
+echo "<br>";
+?>
